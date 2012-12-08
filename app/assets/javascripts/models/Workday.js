c01e3748bc90b6be69c61bf460ec571e4cfebf01
Yana-Krukovets <yanashelli@rambler.ru>
2012-12-08 21:15:35 +0200

    span id of workDay

--- app/assets/javascripts/models/Workday.js
+++ app/assets/javascripts/models/Workday.js
@@ -3,14 +3,13 @@ MTS.Models.WorkDay = Backbone.Model.extend(
 	defaults: 
 	{
 		doc_fio : "Ivanov",
-		duration : 60,
+		duration : 15,
 		from : "13:00",
 		to : "16:00",
 		ticket_time : "8:00",
 		doc_id : 1,
 		data : "Mon",
 		specialization_id : 1,
-		span : "",
 		ticket_t : 0,
 		duration_class : ''
 	}
--- app/assets/javascripts/views/Workday.js
+++ app/assets/javascripts/views/Workday.js
@@ -1,12 +1,23 @@
 MTS.Views.WorkDayView = Backbone.View.extend({
 	template_tr : JST["TemplateTr"],
 	template_span : JST["TimetableTicket"],
-	el_tr : "#tablebody",
+	el_tr : '#tablebody',
 	initialize : function()
 	{
 		//var days = new MTS.Collections.WorkDayCollection(),
 		//days.on("reset", this.render, this); 
 		this.render();      
+	},
+	getCount : function()
+	{
+		var count  = 60/day.get('duration');
+			if(count == 1)
+				day.set('duration_class', 'hour');
+			if(count == 2)
+				day.set('duration_class', 'half');
+			if(count == 4)
+				day.set('duration_class', 'quarter');
+		return count;
 	},
 	render : function()
 	{
@@ -17,31 +28,26 @@ MTS.Views.WorkDayView = Backbone.View.extend({
 		for(j = 0; j <= 10; j++)
 		{
 			time[1] = 0;
-			var count  = 60/day.get('duration');
-			if(count == 1)
-				day.set('duration_class', 'hour');
-			if(count == 2)
-				day.set('duration_class', 'half');
-			if(count == 4)
-				day.set('duration_class', 'quarter');
+			var count = this.getCount();
 			for(i = 0; i < count; i++)
 			{
 				if(time[1] == 0)
-					time[1] = "00";
+					time[1] ="00";
 				if(parseInt(time[0])>=parseInt(from[0])&&parseInt(time[0])<parseInt(to[0]))
-					day.set('ticket_time', time[0] + ':' + time[1]);
+					day.set('ticket_time', time[0]+':' + time[1]);
 				else
 					day.set('ticket_time', '-');
+				day.set('ticket_t', time[0]+'' + time[1]);
 				sp += this.template_span(day.toJSON());
 				duration = day.get('duration') + 0;
 				time[1] = parseInt(time[1]) + duration;
 			}
 			time[0]++;
-			day.set('ticket_time', time[0] + ':' + time[1]);
-			day.set('ticket_t', time[0] + '' + time[1]);
+			if(time[0]/10<1)
+				time[0]='0'+time[0];
+			day.set('ticket_time', time[0]+':' + time[1]);
 		}
-		day.set('span', sp);
-		$(this.el_tr).html(this.template_tr(day.toJSON()));
+		$(this.el_tr).append(this.template_tr(day.toJSON()));
 		$("#td_2_doc__" + day.get("doc_id") + "__" + day.get("data")).append(sp);
 	}
 });

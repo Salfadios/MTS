a12ca4ec683fd5ba3a578e62078cc617bfd4e987
Salfadios <dyadyalesha@bk.ru>
2012-12-08 19:02:33 +0200

    Last .ignore modify

--- app/assets/javascripts/mts.js
+++ app/assets/javascripts/mts.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var MTS = {
 	Models: {},
 	Collections: {},
@@ -10,15 +9,4 @@ var MTS = {
 	{
 		console.log('Application start!');
 	}
-=======
-var MTS = {
-	Models: {},
-	Collections: {},
-	Views: {},
-	Routers: {},
-	
-	initialize: function(){
-		console.log('Application start!');
-	}
->>>>>>> 8ca2efc6d3b99c89023e76781a0d5aa7296365eb
 }
--- app/assets/javascripts/views/TicketCollection.js
+++ app/assets/javascripts/views/TicketCollection.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 MTS.Views.TicketCollectionView = Backbone.View.extend({
 	el: "body",
 	board: new MTS.Collections.TicketCollection(),
@@ -40,47 +39,4 @@ MTS.Views.TicketCollectionView = Backbone.View.extend({
 		this.$("#table_time td").empty();
 		this.board.each(this.showOne);
 	}
-=======
-MTS.Views.TicketCollectionView = Backbone.View.extend({
-	el: "body",
-	events: {
-		"click #table_time td":"addOne"
-	},
-	initialize: function(){
-		Board = new TicketCollection();
-		Board.on("reset", this.render, this);
-		Board.fetch();
-		console.log("Tickets initialized!");
-	},
-	addOne: function(e){
-		Board.create({
-			user_name: "Nochovniy Alexey",
-			address: "Yunih Lenintsev 22/115",
-			doctor_name: "Assanov S.",
-			date: "30.11.2012",
-			time: this.setTime(e)
-		});
-	},
-	setTime: function(e){
-		var tdId = e.target.id,
-		hours = Math.floor(tdId/100),
-		minutes = tdId%100;
-		if(minutes == 0)
-			minutes = "00";
-		return hours + ":" + minutes;
-	},
-	render:function(){
-		var view, sticker, i, max = Board.models.length;
-		this.$("#table_time td").empty();
-		for (i = 0; i < max; i++)
-		{
-			sticker = Board.models[i];
-			console.log(sticker);
-			view = new MTS.Views.TicketViewSmall({model:sticker});
-			var time = view.model.get('time').split(':');
-			//var id = '#' + time[0] + '' + time[1];
-			view.render(".app");
-		}
-	}
->>>>>>> 8ca2efc6d3b99c89023e76781a0d5aa7296365eb
 });

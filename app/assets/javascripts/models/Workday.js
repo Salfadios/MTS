4a20f22f2e18a54ac960ab5946874f1a6fe6f368
ticketservice <sticker@ukr.net>
2012-12-13 21:33:50 +0200

    some interim changes about doctor (specialisation_id)

--- app/assets/javascripts/models/Workday.js
+++ app/assets/javascripts/models/Workday.js
@@ -9,7 +9,7 @@ MTS.Models.WorkDay = Backbone.Model.extend(
 		ticket_time : "8:00",
 		doc_id : 1,
 		data : "Mon",
-		specialization_id : 1,
+		specializationid : 1,
 		ticket_t : 0,
 		duration_class : ''
 	}

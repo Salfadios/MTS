a4d0a3e3ec841fc260e9035823969f1a121b0e67
ticketservice <sticker@ukr.net>
2012-12-14 23:13:52 +0200

    SESSION IS WORKiiiiiiiiiiNG !!! :)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Mts::Application.routes.draw do
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  
   resources :users
   resources :sessions
 
